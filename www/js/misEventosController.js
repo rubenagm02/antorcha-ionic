21501f2395916a394851ea61129a9a70d8de6d0e
Ruben <ruben.agm02@gmail.com>
2016-05-26 22:07:11 -0500

    Se agrega un controlador

--- www/js/misEventosController.js
+++ www/js/misEventosController.js
@@ -3,4 +3,8 @@ var app = angular.module('app.controllers', [])
 
 .controller('misEventosCtrl', function($scope) {
 	$scope.eventos = JSON.parse(window.localStorage.getItem("eventos"));
+
+	$scope.invitar = function (evento) {
+		//Se genera el código.
+	}
 })
